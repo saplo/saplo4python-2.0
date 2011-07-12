76e8f9a4bf6194dcb4565ed1eb81d4cdab850ad8
Rogelio <rogeliorv@gmail.com>
2011-07-12 17:44:07 -0500

    Added readme, reorganized packages and made saploapi to use simplejson which is better.

---
+++ pysaplo/__init__.py
@@ -0,0 +1,7 @@
+'''
+Created on Jul 12, 2011
+
+@author: rogelio
+'''
+
+from pysaplo import saploapi
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name="pysaplo",
+      version="1.0.0",
+      description="Python Library for the Saplo API",
+      license="MIT",
+      author="rogeliorv",
+      author_email="",
+      url="http://github.com/rogeliorv/pysaplo",
+      packages = find_packages(),
+      keywords= "saplo library",
+      zip_safe = True)
